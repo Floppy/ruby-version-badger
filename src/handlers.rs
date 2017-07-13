00bee261549de2b561b80687f7a33e94c25f4221
James Smith <james@floppy.org.uk>
2017-07-14 00:05:59 +0100

    Fetch the Gemfile for a repository

--- src/handlers.rs
+++ src/handlers.rs
@@ -1,6 +1,7 @@
 use iron::prelude::*;
 use iron::status;
 use router::Router;
+use https;
 
 pub fn github(req: &mut Request) -> IronResult<Response> {
     
@@ -8,9 +9,10 @@ pub fn github(req: &mut Request) -> IronResult<Response> {
     let ref user = req.extensions.get::<Router>().unwrap().find("user").unwrap_or("");
     let ref repo = req.extensions.get::<Router>().unwrap().find("repo").unwrap_or("");
         
-    
+    // Get a file
+    let url = String::from(format!("https://raw.githubusercontent.com/{}/{}/master/Gemfile", user, repo));
+    let gemfile = https::get(url);
     
     // Send response
-    let str = format!("Generating badge for github.com/{}/{}", user, repo);
-    Ok(Response::with((status::Ok, str)))
+    Ok(Response::with((status::Ok, gemfile)))
 }
