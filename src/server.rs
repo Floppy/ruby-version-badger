66646b220c5b88c9b12bacb62d60c0e84499513c
James Smith <james@floppy.org.uk>
2017-07-13 21:22:21 +0100

    split serve method up

--- src/server.rs
+++ src/server.rs
@@ -3,17 +3,16 @@ use router::Router;
 use config;
 use handlers;
 
-pub fn serve() {
-    
-    // build routes
-    let mut router = Router::new();
-    router.get("/", handlers::root, "index");
-    router.get("/github/:user/:repo", handlers::github, "github");
-
-    // serve
+pub fn serve() {    
     let interface = format!("0.0.0.0:{}", config::port());
     println!("Binding on {:?}", interface);
-    Iron::new(router).http(&interface[..]).unwrap();
+    Iron::new(router()).http(&interface[..]).unwrap();
     println!("Bound on {:?}", interface);
+}
 
+fn router () -> Router {
+    let mut r = Router::new();
+    r.get("/", handlers::root, "index");
+    r.get("/github/:user/:repo", handlers::github, "github");    
+    return r;
 }
