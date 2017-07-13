07f29f09c8961f5ec973cd15c76e6007fc108151
James Smith <james@floppy.org.uk>
2017-07-13 16:27:51 +0100

    bind to port from env

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,23 @@ extern crate iron;
 
 use iron::prelude::*;
 use iron::status;
+use std::env;
 
 fn main() {
-    Iron::new(|_: &mut Request| {
+
+    fn hello_world(_: &mut Request) -> IronResult<Response> {
         Ok(Response::with((status::Ok, "Hello World!")))
-    }).http("localhost:3000").unwrap();
+    }
+
+    let mut port = String::from("3000");
+    match env::var("PORT") {
+        Ok(val) => port = val,
+        Err(_e) => println!("using default port {}",  port),
+    }
+
+    let url = format!("0.0.0.0:{}", port);
+
+    println!("Binding on {:?}", url);
+    Iron::new(hello_world).http(&url[..]).unwrap();
+    println!("Bound on {:?}", url);
 }
