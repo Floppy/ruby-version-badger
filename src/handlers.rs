0df9fa138b495331de38d45670909416303128a2
James Smith <james@floppy.org.uk>
2017-07-13 22:24:35 +0100

    get params for github repo

--- src/handlers.rs
+++ src/handlers.rs
@@ -3,7 +3,14 @@ use iron::status;
 use router::Router;
 
 pub fn github(req: &mut Request) -> IronResult<Response> {
+    
+    // Get user and repo
     let ref user = req.extensions.get::<Router>().unwrap().find("user").unwrap_or("");
     let ref repo = req.extensions.get::<Router>().unwrap().find("repo").unwrap_or("");
-    Ok(Response::with((status::Ok, "github handler")))
+        
+    
+    
+    // Send response
+    let str = format!("Generating badge for github.com/{}/{}", user, repo);
+    Ok(Response::with((status::Ok, str)))
 }
