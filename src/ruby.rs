52d0e76fac8c62c1dfde7fcbb6360d119dd921a3
James Smith <james@floppy.org.uk>
2017-07-17 15:39:59 +0100

    refactor to use a match for ruby version checking
    As recommended in #1

--- src/ruby.rs
+++ src/ruby.rs
@@ -1,24 +1,13 @@
 pub fn colour(version: String) -> String {
-    // Check version and set colour
-    println!("version being checked: '{}'", version);
-    let mut colour = "red";
-    if version == "2.4.1" {
-        // current
-        colour = "brightgreen";
-    }
-    else if version == "2.3.4" {
-        // previous but in lifetime
-        colour = "yellow";
-    }
-    else if version == "2.2.7" {
-        // approaching EOL
-        colour = "orange";
-    }
-    else if version == "" || version == "404: Not Found" {
-        // unknown
-        colour = "lightgray";
-    }
-    return String::from(colour);
+    // Check version and set colour    
+    match version.as_ref() {
+        "2.4.1"          => "brightgreen",
+        "2.3.4"          => "yellow",
+        "2.2.7"          => "orange",
+        ""               => "lightgray",
+        "404: Not Found" => "lightgray",
+        _                => "red",
+    }.to_string()
 }
 
 #[cfg(test)]
