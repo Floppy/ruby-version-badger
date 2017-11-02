7dbb5eeb7a634556668fbf2dacf63352454b2e63
James Smith <james@floppy.org.uk>
2017-11-02 13:02:38 +0000

    update ro rust 1.21.0

--- src/rust.rs
+++ src/rust.rs
@@ -1,7 +1,7 @@
 pub fn colour(version: String) -> String {
     // Check version and set colour    
     match version.as_ref() {
-        "1.20.0"         => "brightgreen",
+        "1.21.0"         => "brightgreen",
         ""               => "lightgray",
         "404: Not Found" => "lightgray",
         _                => "red",
@@ -19,6 +19,6 @@ mod tests {
 
     #[test]
     fn current_versions_are_green() {
-        assert_eq!("brightgreen", rust::colour(String::from("1.20.0")));
+        assert_eq!("brightgreen", rust::colour(String::from("1.21.0")));
     }
 }
