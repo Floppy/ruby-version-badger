4faf7a26728f6ab7ae5202ce7c3b30adb377c0e5
James Smith <james@floppy.org.uk>
2020-06-19 15:51:17 +0100

    use blocking IO on reqwest
    I'm too lazy to work out async in rust right now

--- src/github.rs
+++ src/github.rs
@@ -2,12 +2,12 @@ use reqwest;
 
 pub fn exists(user: &String, repo: &String, branch: &String, filename: &String) -> Result<bool, reqwest::Error> {
     let url = format!("https://raw.githubusercontent.com/{}/{}/{}/{}", user, repo, branch, filename);
-    let resp = reqwest::get(url.as_str())?;
+    let resp = reqwest::blocking::get(url.as_str())?;
     return Ok(resp.status().is_success());
 }
 
 pub fn get(user: &String, repo: &String, branch: &String, filename: &String) -> Result<String, reqwest::Error> {
     let url = format!("https://raw.githubusercontent.com/{}/{}/{}/{}", user, repo, branch, filename);
-    let file = reqwest::get(url.as_str())?.text()?;
+    let file = reqwest::blocking::get(url.as_str())?.text()?;
     return Ok(file);
 }
